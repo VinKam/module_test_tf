6dc6acf7f74e92030f5a3db720c14bd3a9e9b266
vinkam <vinkam8@gmail.com>
2020-11-09 03:08:56 -0500

    Module to create a instance

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "ami" {
+  type = string
+  description = "ami for instance"
+}
+
+variable "instance_type" {
+  type = string
+  description = "Instance type"
+}
+
+variable "instance_name" {
+  type = string
+  description = "describe your variable"
+}
+
